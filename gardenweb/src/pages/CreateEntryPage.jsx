c27a1bb9090a38008b489565053e5300fe152cc2
Sandra Yee <sandra.yee001@gmail.com>
2023-11-28 06:44:54 -0500

    add create entry page and functionality

---
+++ gardenweb/src/pages/CreateEntryPage.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { supabase } from '../client.js'
+
+const CreateEntryPage = () => {
+    // state variable to hold user input data (must be "" to use .substring())
+    const [post, setPost] = useState({title: "", description: "", imgURL: ""})
+
+    // update post state variable with form inputs
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setPost( (prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        })
+    }
+
+    // insert entry post data to supabase database
+    const createPost = async (event) => {
+        event.preventDefault();
+
+        const { error } = await supabase
+        .from('Entries')
+        .insert({title: post.title, description: post.description, imgURL: post.imgURL})
+        .select()
+
+        console.log("inserted to entries")
+
+        if (error) {
+            console.log(error);
+        }
+        
+        // route to this homepage after submit
+        window.location = "/";
+
+    }
+
+    return (
+        <div>
+            <h1 className="post-grid">Create New Journal Entry</h1>
+            <form>
+                <label>Title</label>
+                <input type="text" id="title" name="title" value ={post.title} onChange={handleChange}/>
+
+                <label >Description</label>
+                <textarea name="description" rows="5" cols="50" id="description" value ={post.description} onChange={handleChange}>
+                </textarea>
+
+                <label >Image URL (optional)</label><br />
+                <input type="text" id="imgURL" name="imgURL" value ={post.imgURL} onChange={handleChange}/>
+
+                <input type="submit" value="Create New Journal Entry" onClick={createPost} />
+            </form>
+        </div>
+    )
+}
+
+export default CreateEntryPage
