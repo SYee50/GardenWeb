e8bf832d14978bff3cec20ae88c1ce469244f2b4
Sandra Yee <sandra.yee001@gmail.com>
2023-12-06 02:19:00 -0500

    search journal entries by title

--- gardenweb/src/App.jsx
+++ gardenweb/src/App.jsx
@@ -1,24 +1,38 @@
 import './App.css'
 import ReadEntries from './components/ReadEntries'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function App() {
+  const [searchInput, setSearchInput] = useState("")
+
   return (
     <div>
+
+      <h1 id="home-title" >Journal Entries</h1>
+
       {/* search bar and crate button */}
       <div style={{display: 'flex', width: '90%', margin: "20px auto", borderRadius: "15px"}}>
           <div style={{display: 'flex', width: '75%'}}>
-            <input style={{width: '75%', marginRight: "10px"}} type="search" placeholder="Search" aria-label="Search" />
-            <button style={{width: '15%'}} type="submit">Search</button>
+            <input 
+              style={{width: '75%', marginRight: "10px"}} 
+              type="search" 
+              placeholder="Search" 
+              aria-label="Search" 
+              
+              // get search input
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+
           </div>
           <Link to={'/create'} >
             <button>Create New Journal Entry</button>
           </Link>
       </div>
-
-      <h1 id="home-title" >Journal Entries</h1>
       
-      <ReadEntries />
+      {/* pass search input to component for post list */}
+      <ReadEntries search={searchInput} />
     </div>
   )
 }
--- gardenweb/src/components/ReadEntries.jsx
+++ gardenweb/src/components/ReadEntries.jsx
@@ -3,8 +3,10 @@ import { supabase } from '../client'
 import { Link } from "react-router-dom"
 import Entry from "./Entry";
 
-const ReadEntries = () => {
+const ReadEntries = (props) => {
     const [posts, setPosts] = useState([]);
+
+    console.log(props.search)
 
     useEffect(() => {
       // READ all rows from Entries table
@@ -21,17 +23,29 @@ const ReadEntries = () => {
       fetchPosts()
     }, []);
 
+    // indicate if the search input is present in a journal entry's title
+    const matchSearchInput = (post) => {
+      return post.title.toLowerCase().includes(props.search.toLowerCase())
+    }
+
     return (
         <div className="ReadPosts">
             {/* map out each row of data to an Entry component */}
+            {/* filter posts by title when user types in search bar */}
             {
-                posts && posts.length > 0 ?
-                posts.map((post,index) => 
-                // user can click on each Entry post to go to a separate page with more content
-                <Link to={"/more/" + post.id}>
-                   <Entry key={post.id} id={post.id} title={post.title} time={post.created_at} description={post.description} imgURL={post.imgURL}/>
-                </Link>
-                ) : <h3 className="noResults">{'No Entries Yet 😞'}</h3>
+              posts && posts.length > 0 ?
+                posts
+                  .filter(matchSearchInput)
+                  .map((post) =>
+                    <Link to={"/more/" + post.id}>
+                      <Entry key={post.id} 
+                             id={post.id} 
+                             title={post.title} 
+                             time={post.created_at} 
+                             description={post.description} 
+                             imgURL={post.imgURL}/>
+                    </Link>
+                  ) : <h3 className="noResults">{'No Entries Yet'}</h3>
             }
         </div>  
     )
